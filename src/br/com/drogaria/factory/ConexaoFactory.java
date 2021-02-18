67db49ccde0f69c22e2e08aa6c152387c768891f
Luan Heuripedes <heuripedesmoreiradepaula@gmail.com>
2021-02-18 00:20:20 -0300

    Retirado informações referentes a conexão

--- src/br/com/drogaria/factory/ConexaoFactory.java
+++ src/br/com/drogaria/factory/ConexaoFactory.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoFactory {
-	private static final String USUARIO = "root";
-	private static final String SENHA = "123456789";
-	private static final String URL = "jdbc:mysql://localhost:3306/drogaria";
+	private static final String USUARIO = "";
+	private static final String SENHA = "";
+	private static final String URL = "";
 
 	public static Connection conectar() throws SQLException {
 		//Se ocorrer error
